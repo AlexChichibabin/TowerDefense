1fe3f8b7eefc50de007fb765a437c26b495a6d3b
AlexChichibabin <Wolfrahm57@mail.ru>
2023-09-01 17:34:13 +0300

    Реализовали интерактивные панели покупки башен (Перемещение, появление и уход, но не построение башен)

---
+++ Assets/Scripts/NullBuildSite.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+namespace TowerDefense
+{
+    public class NullBuildSite : BuildSite
+    {
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            InvokeNullEvent();
+        }
+    }
+}
