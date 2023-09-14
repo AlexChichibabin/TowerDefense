60b9dd74826cfdf9da1f8f2ce4276ae9049a3a03
AlexChichibabin <Wolfrahm57@mail.ru>
2023-09-14 18:42:41 +0300

    Реализована остановка всех объектов при проигрыше и рестарт по нажатию кнопки

--- Assets/Scripts/TDLevelController.cs
+++ Assets/Scripts/TDLevelController.cs
@@ -19,7 +19,22 @@ namespace TowerDefense
 
         private void StopLevelActivity()
         {
-            Debug.Log("Stop");
+            foreach (var enemy in FindObjectsOfType<Enemy>())
+            {
+                enemy.GetComponent<Ship>().enabled = false;
+                enemy.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            }
+            void DisableAll<T>() where T : MonoBehaviour
+            {
+                foreach (var obj in FindObjectsOfType<T>())
+                {
+                    obj.enabled = false;
+                }
+            }
+            DisableAll<Spawner>();
+            DisableAll<Projectile>();
+            DisableAll<Tower>();
+
         }
     }
 }
