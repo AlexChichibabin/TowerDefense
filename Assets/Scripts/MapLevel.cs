c009fd2c4b1349db9eed937caac20fcb5c4eeae8
AlexChichibabin <Wolfrahm57@mail.ru>
2023-09-21 20:02:10 +0300

    Изменения в архитектуре иконок уровней

--- Assets/Scripts/MapLevel.cs
+++ Assets/Scripts/MapLevel.cs
@@ -8,9 +8,21 @@ namespace TowerDefense
     public class MapLevel : MonoBehaviour
     {
         [SerializeField] private Episode episode;
+        [SerializeField] private bool isLeftSided;
         public void LoadLevel()
         {
             LevelSequenceController.Instance.StartEpisode(episode);
         }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (isLeftSided)
+            {
+                
+            }
+            
+        }
+#endif
     }
 }
